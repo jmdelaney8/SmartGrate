4cf4225f771ff9c8be084e635334166ab589514a
Jim Delaney <deljm@umich.edu>
2019-01-12 00:54:45 -0500

    RPi prints serial without converting to int

--- serial_test.py
+++ serial_test.py
@@ -5,7 +5,5 @@ ser.baudrate = 9600
 ser.write(b'10')
 s = [0]
 while True:
-    read_serial = int(ser.readline(), 16)
-    s[0] = int(ser.readline(), 16)
-    print(s[0])
+    read_serial = ser.readline()
     print(read_serial)
