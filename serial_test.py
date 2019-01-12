e1cbe8face8c3c505ab6dd5e3e7f74099d88e6ae
Jim Delaney <deljm@umich.edu>
2019-01-12 00:58:54 -0500

    Print current time before serial

--- serial_test.py
+++ serial_test.py
@@ -1,4 +1,5 @@
 import serial
+import datetime as dt
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 ser.baudrate = 9600
@@ -6,4 +7,5 @@ ser.write(b'10')
 s = [0]
 while True:
     read_serial = ser.readline()
+    print('{}:').format(dt.now())
     print(read_serial)
