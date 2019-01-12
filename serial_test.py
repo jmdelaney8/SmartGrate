d122b1a639d2332c4e78b7778fba351930fff351
Jim <jmdelaney8@gmail.com>
2019-01-12 00:36:41 -0500

    Add misc serial test files

---
+++ serial_test.py
@@ -0,0 +1,13 @@
+import serial
+
+ser = serial.Serial('/dev/ttyACM1', 9600)
+ser.baudrate = 9600
+ser.write(b'10')
+s = [0]
+"""
+while True:
+    read_serial = int(ser.readline(), 16)
+    s[0] = int(ser.readline(), 16)
+    print(s[0])
+    print(read_serial)
+    """
